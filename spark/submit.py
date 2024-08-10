da4f3826fe0bdef8b56d24d281c4fdcd44b64650
Dan Snow <dan@sno.ws>
2024-08-10 00:37:10 +0000

    Refactor spark test to class

---
+++ spark/submit.py
@@ -0,0 +1,26 @@
+from .spark_app import SparkApp
+from pathlib import Path
+import csv
+
+
+def read_predicates_from_csv(path: Path) -> list[str]:
+    predicates = []
+    with open(path.resolve().as_posix(), mode="r") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            start, end, _ = row
+            predicate = f"parid >= '{start}' AND parid <= '{end}'"
+            predicates.append(predicate)
+    return predicates
+
+
+predicates = read_predicates_from_csv(Path("/tmp/spark/predicates.csv"))
+
+addn = SparkApp(
+    app_name="addn",
+    table_name="addn",
+    taxyr=["2019"],
+    cur=["Y"],
+    predicates=predicates,
+)
