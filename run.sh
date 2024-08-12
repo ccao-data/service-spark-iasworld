2feb7db4607ac46593b41cbf77a251590a65f91e
Dan Snow <31494343+dfsnow@users.noreply.github.com>
2024-08-12 16:06:40 -0500

    Update run.sh
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Example command to execute test jobs in the Spark cluster
-# First run `docker compose up` in the repo root
+# First run `docker compose up -d` in the repo root
 docker exec -it spark-node-master \
     ./submit.sh --json-string \
     "$(yq -o=json .test_jobs ./config/default_jobs.yaml)"
