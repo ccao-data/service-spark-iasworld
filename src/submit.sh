9e90c6a0ebf1b43506aa729689a2cfc2849f83aa
Dan Snow <daniel.snow@cookcountyil.gov>
2024-11-14 01:27:04 +0000

    Set executor memory at runtime

--- src/submit.sh
+++ src/submit.sh
@@ -4,4 +4,7 @@
 # Spark application (group of jobs). Note that it must be passed as an integer,
 # hence the double parenthesis. Note also that setting this statically means
 # we can only run one Spark application at a time (per environment)
-spark-submit --conf "spark.ui.port=$((SPARK_UI_PORT))" /tmp/src/submit_jobs.py "$@"
+spark-submit \
+    --conf "spark.ui.port=$((SPARK_UI_PORT))" \
+    --conf "spark.executor.memory=${SPARK_EXECUTOR_MEMORY}" \
+    /tmp/src/submit_jobs.py "$@"
